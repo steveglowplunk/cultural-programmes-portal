d07a005f76cda95643327f3fb68233f3413df4a6
steveglowplunk <28670916+steveglowplunk@users.noreply.github.com>
2024-12-17 04:16:18 +0800

    Add map

--- app/(main)/event-info/page.tsx
+++ app/(main)/event-info/page.tsx
@@ -1,30 +1,24 @@
 "use client";
 
-import { useEffect } from "react";
-import maplibregl from 'maplibre-gl';
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 const EventInfo = () => {
-  useEffect(() => {
-    const key = process.env.NEXT_PUBLIC_MAPTILER_KEY;
-    const map = new maplibregl.Map({
-      container: 'map',
-      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${key}`,
-      center: [114.222075, 22.285056],
-      zoom: 6
-    });
-    const london = new maplibregl.Marker()
-      .setLngLat([114.222075, 22.285056])
-      .addTo(map);
-    map.on('error', function(err) {
-      throw new Error("To load the map, you must replace YOUR_MAPTILER_API_KEY_HERE first, see README");
-    });
-  }, []);
+  const Map = useMemo(() => dynamic(
+    () => import('@/components/map/'),
+    {
+      loading: () => <p>A map is loading</p>,
+      ssr: false
+    }
+  ), [])
 
   return (
     <>
-        <div id="map" className="absolute top-0 bottom-0 w-full h-full"></div>
+      <div className="h-[600px]">
+        <Map posix={[22.285056, 114.222075]} posix2={[22.44152, 114.02289]} />
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default EventInfo;
+export default EventInfo
