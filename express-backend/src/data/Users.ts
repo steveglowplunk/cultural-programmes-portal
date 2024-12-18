407464445601abe11d500ecd0b6a68a789ef5a68
NobikaNg <ng1936787098@gmail.com>
2024-12-18 22:43:57 +0800

    feat: admin_user_delete

--- express-backend/src/data/Users.ts
+++ express-backend/src/data/Users.ts
@@ -1,96 +1,93 @@
 export const users = [
   {
-    "username": "admin",
-    "password": "abc123",
-    "email": "admin@example.com",
-    "role": "normal",
-    "favouriteVenues": [
-      "3110565",
-      "3110267"
-    ]
+    username: "admin",
+    password: "abc123",
+    email: "admin@example.com",
+    role: "normal",
+    favouriteVenues: ["3110565", "3110267"],
   },
   {
-    "username": "admin1",
-    "email": "admin1@123.com",
-    "password": "123456",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "admin1",
+    email: "admin1@123.com",
+    password: "123456",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "admin1123",
-    "email": "123121@fdwopq.com",
-    "password": "123456",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "admin1123",
+    email: "123121@fdwopq.com",
+    password: "123456",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "testuser1",
-    "email": "testuser1@example.com",
-    "password": "password123",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "testuser1",
+    email: "testuser1@example.com",
+    password: "password123",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "testuser2",
-    "email": "testuser2@example.com",
-    "password": "mypassword",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "testuser2",
+    email: "testuser2@example.com",
+    password: "mypassword",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "testuser3",
-    "email": "testuser3@example.com",
-    "password": "securepass",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "testuser3",
+    email: "testuser3@example.com",
+    password: "securepass",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "testuser4",
-    "email": "testuser4@example.com",
-    "password": "letmein",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "testuser4",
+    email: "testuser4@example.com",
+    password: "letmein",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "testuser5",
-    "email": "testuser5@example.com",
-    "password": "12345678",
-    "favouriteVenues": [],
-    "role": "admin"
+    username: "testuser5",
+    email: "testuser5@example.com",
+    password: "12345678",
+    favouriteVenues: [],
+    role: "admin",
   },
   {
-    "username": "testuser6",
-    "email": "testuser6@example.com",
-    "password": "193678",
-    "favouriteVenues": [],
-    "role": "admin"
+    username: "testuser6",
+    email: "testuser6@example.com",
+    password: "193678",
+    favouriteVenues: [],
+    role: "admin",
   },
   {
-    "username": "user7",
-    "email": "user7@example.com",
-    "password": "qwertyuiop",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "user7",
+    email: "user7@example.com",
+    password: "qwertyuiop",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "testuser8",
-    "email": "testuser8@example.com",
-    "password": "zxcvbnm",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "testuser8",
+    email: "testuser8@example.com",
+    password: "zxcvbnm",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "13223121",
-    "email": "41223r1@vfds.com",
-    "password": "123456",
-    "favouriteVenues": [],
-    "role": "normal"
+    username: "13223121",
+    email: "41223r1@vfds.com",
+    password: "123456",
+    favouriteVenues: [],
+    role: "normal",
   },
   {
-    "username": "I am become death destroyer of world",
-    "email": "nuclear@bomb.com",
-    "password": "1234TAT",
-    "favouriteLocations": [],
-    "role": "admin"
-  }
+    username: "I am become death destroyer of world",
+    email: "nuclear@bomb.com",
+    password: "1234TAT",
+    favouriteLocations: [],
+    role: "admin",
+  },
 ];
